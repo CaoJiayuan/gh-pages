e84d38cc586da1917c0c2e37228475d8741163c3
cjy <cjy632258@hotmail.com>
2018-07-24 15:01:03 +0800

    try again

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+set -e
+
+npm run build
+
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://${GITHUB_TOKEN}@github.com/caojiayuan/caojiayuan.github.io.git gh-pages
+
+cd -
